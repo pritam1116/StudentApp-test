de36883f60295be01d12a32a7c77b1df6ccbcab9
Pritam Sinha <Pritam Sinha@DESKTOP-4M9TNSB>
2020-05-10 22:22:00 +0530

    fixed everything

---
+++ web-student-tracker/src/com/luv2code/web/jdbc/TestDeploy.java
@@ -0,0 +1,11 @@
+package com.luv2code.web.jdbc;
+
+public class TestDeploy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Succefully Deployed!");
+
+	}
+
+}
